bc89d38edec5f250f8043f6f747084c3f472793d
Yijun Tay <tay.yijun@gmail.com>
2021-04-06 19:18:31 +0800

    [DS-14] bulk write to elasticsearch

--- documentdb_to_es_loader/es_loader.py
+++ documentdb_to_es_loader/es_loader.py
@@ -1,38 +1,89 @@
-import pymongo
-import elasticsearch
-
 import json
 
+from elasticsearch import Elasticsearch, helpers, ConnectionError
+from elasticsearch.helpers import BulkIndexError
+from pymongo import MongoClient
+from pymongo.cursor import Cursor
+from pymongo.errors import ServerSelectionTimeoutError
 
-def get_mongo_client(config: dict) -> pymongo.MongoClient:
-    host = config["MONGODB"]["HOST"]
-    username = config["MONGODB"]["USERNAME"]
-    password = config["MONGODB"]["PASSWORD"]
-    return pymongo.MongoClient(
+
+def get_mongo_client(config: dict) -> MongoClient:
+    host = config["MONGO"]["HOST"]
+    username = config["MONGO"]["USERNAME"]
+    password = config["MONGO"]["PASSWORD"]
+    db = config["MONGO"]["DB"]
+    mongo_client = MongoClient(
         host,
         username=username,
         password=password,
+        authSource=db,
     )
+    try:
+        # test connection to mongo client
+        mongo_client.admin.command("ismaster")
+        print("Success! Connected to MongoDB.")
+        return mongo_client
+    except ServerSelectionTimeoutError:
+        print("Error! Failed to connect to MongoDB.")
 
 
-def get_document(client: pymongo.MongoClient) -> str:
-    document_db = client["docu_search"]
-    collection = document_db["documents"]
-    # document = collection.find_one()
-    # return document
-    return collection.full_name
+def get_mongo_documents(client: MongoClient, bulk_size: int = 5) -> list[Cursor]:
+    try:
+        document_db = client["docu_search"]
+        collection = document_db["documents"]
+        mongo_documents = list(collection.find().sort("_id").limit(bulk_size))
+        print("Success! Retrieved documents from MongoDB.")
+        return mongo_documents
+    except:
+        print("Error! Failed to get documents from MongoDB.")
 
 
-def get_es_client(config: dict) -> elasticsearch.Elasticsearch:
+def get_es_client(config: dict) -> Elasticsearch:
     host = config["ES"]["HOST"]
     username = config["ES"]["USERNAME"]
     password = config["ES"]["PASSWORD"]
-
-    return elasticsearch.Elasticsearch(
+    elastic_client = Elasticsearch(
         host,
         http_auth=(username, password),
         scheme="https",
     )
+    try:
+        # test connection to elasticsearch client
+        elastic_client.nodes.info()
+        print("Success! Connected to Elasticsearch.")
+        return elastic_client
+    except ConnectionError:
+        print("Error! Failed to connect to Elasticsearch.")
+
+
+def build_es_actions(mongo_documents: list[Cursor], config: dict) -> list[dict]:
+    index = config["ES"]["INDEX"]
+    es_actions = []
+    for document in mongo_documents:
+        es_actions.append(
+            {
+                "_op_type": "create",
+                "_index": index,
+                "_type": "_doc",
+                "_id": str(document["_id"]),
+                "doc": {
+                    "topic": document["topic"],
+                    "content": document["content"],
+                },
+            }
+        )
+    return es_actions
+
+
+def bulk_create_es_documents(client: Elasticsearch, actions: list[dict]):
+    try:
+        helpers.bulk(client, actions)
+        print("Success! Created ES documents.")
+    except BulkIndexError as exception:
+        print("Error! Failed to run bulk create on Elasticsearch.")
+        errors = exception.errors
+        errorIds = [d["create"]["_id"] for d in errors]
+        print("IDs of failed documents: {}".format(errorIds))
 
 
 def read_json(file_path):
@@ -44,8 +95,11 @@ if __name__ == "__main__":
     config = read_json("config.json")
 
     mongo_client = get_mongo_client(config)
-    one_document = get_document(mongo_client)
-    print(one_document)
+    mongo_documents = get_mongo_documents(mongo_client)
+    actions = build_es_actions(mongo_documents, config)
 
     es_client = get_es_client(config)
-    print(es_client.indices.exists("doc-search-01"))
+    bulk_create_es_documents(es_client, actions)
+
+    print()
+    print("End.")
