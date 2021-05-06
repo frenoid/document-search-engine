fbbf5c720e76ae81e30819279cc103777e422d8b
joswx <weixin0121@gmail.com>
2021-05-06 22:06:25 +0800

    Added Terraform config for security group, s3, lambda and es

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,11 @@
 # ------------------------------------------------------------------------------
 # CREATE EC2 Instances
 # ------------------------------------------------------------------------------
+# GCP to S3 Loader service
 resource "aws_instance" "gcp-to-s3-loader-service" {
     ami = "ami-0fab0953c3bb514a9"
     instance_type = "t2.micro"
-    key_name = "nus-iss-group3"
+    key_name = var.ec2_key_name
     security_groups = ["<security_group_here"]
 
     tags = {
@@ -12,43 +13,7 @@ resource "aws_instance" "gcp-to-s3-loader-service" {
     }
   
 }
-
-resource "aws_instance" "doc-search-backend" {
-    ami = "ami-05b10160400f5d51e"
-    instance_type = "t2.micro"
-    key_name = "nus-iss-group3"
-    security_groups = ["<security_group_here"]
-
-    tags = {
-        Name = "doc-search-backend"
-    }
-  
-}
-
-resource "aws_instance" "document-db" {
-    ami = "ami-0d97c93b6ac5342f6"
-    instance_type = "t2.micro"
-    key_name = "nus-iss-group3"
-    security_groups = ["<security_group_here"]
-
-    tags = {
-        Name = "document-db"
-    }
-  
-}
-
-resource "aws_instance" "djangoapp" {
-    ami = "ami-03ca998611da0fe12"
-    instance_type = "t2.micro"
-    key_name = "nus-iss-group3"
-    security_groups = ["<security_group_here"]
-
-    tags = {
-        Name = "djangoapp"
-    }
-  
-}
-
+# Elastic Search loader service
 resource "aws_instance" "es-loader-service" {
     ami = "ami-0fab0953c3bb514a9"
     instance_type = "t2.micro"
@@ -58,9 +23,90 @@ resource "aws_instance" "es-loader-service" {
     tags = {
         Name = "es-loader-service"
     }
+
+}
+# Document DB
+resource "aws_instance" "document-db" {
+    ami = "ami-0d97c93b6ac5342f6"
+    instance_type = "t2.micro"
+    key_name = var.ec2_key_name
+    security_groups = ["<security_group_here"]
+
+    tags = {
+        Name = "document-db"
+    }
   
 }
+# Document Search Backend Service
+resource "aws_instance" "doc-search-backend" {
+    ami = "ami-05b10160400f5d51e"
+    instance_type = "t2.micro"
+    key_name = var.ec2_key_name
+    security_groups = ["<security_group_here"]
 
+    tags = {
+        Name = "doc-search-backend"
+    }
+  
+}
+# Django app server
+resource "aws_instance" "djangoapp" {
+    ami = "ami-03ca998611da0fe12"
+    instance_type = "t2.micro"
+    key_name = var.ec2_key_name
+    security_groups = ["<security_group_here"]
+
+    tags = {
+        Name = "djangoapp"
+    }
+
+}
+# ------------------------------------------------------------------------------
+# CREATE Elastic Search Domain
+# ------------------------------------------------------------------------------
+resource "aws_elasticsearch_domain" "doc-search" {
+    domain_name = "doc-search"
+    elasticsearch_version = "7.9"
+
+    cluster_config {
+        instance_type = "t3.small.elasticsearch"
+        instance_count = 1
+    }
+
+    ebs_options {
+       ebs_enabled = true
+       volume_size = 10
+    }
+
+    tags = {
+        Domain = "doc-search"
+    }
+
+}
+
+# ------------------------------------------------------------------------------
+# CREATE S3 Instance
+# ------------------------------------------------------------------------------
+resource "aws_s3_bucket" "build-artifacts" {
+    bucket = "build-artifacts-bucket" 
+    acl = "private"
+
+    tags = {
+        name = "group3-build-artifacts"
+        Environment = "Dev"
+    }
+} 
+
+# ------------------------------------------------------------------------------
+# CREATE Lambda Functions
+# ------------------------------------------------------------------------------
+resource "aws_lambda_function" "mongo-loader-function" {
+    function_name = "mongo-loader-function"
+    role = var.lambda_arn_role
+    rntime = "python3.8"
+    memory_size = 512
+    timeout = 30
+}
 # ------------------------------------------------------------------------------
 # CREATE AWS Security Group
 # ------------------------------------------------------------------------------
