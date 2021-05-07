1a26e0a1e7eecfbc7034fd5bce4ac0de32064a84
Yijun Tay <tay.yijun@gmail.com>
2021-05-08 02:14:11 +0800

    feat: add get file by id endpoint

--- web-application/backend/src/apps/search/views.py
+++ web-application/backend/src/apps/search/views.py
@@ -30,34 +30,33 @@ def search(request):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 @require_GET
-def retrieve_file_details(request):
-    file_id = request.GET.get("id")
-
-    if file_id:
-        try:
-            response = search_document_by_key(file_id)
-            return JsonResponse(response, status=status.HTTP_200_OK)
-        except Exception:
-            return JsonResponse({ "error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    else:
-        return JsonResponse({ "error": "Search criteria cannot be null"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-@require_POST
 def retrieve_file_details(request, id):
-    data=request.data
+    """
+    <str:id> - document id
+    """
 
-    if not (data.type or data.id):
-        return JsonResponse({ "error": "Search criteria cannot be null"}, status=status.HTTP_400_BAD_REQUEST)
-    #todo: to be replaced 
-    count = 10;
-    if data.type == "like":
-        count =  count - 1
-    else:
-        count = count + 1
-    return JsonResponse({ votes: count }, status=status.HTTP_200_OK)
+    try:
+        response = search_document_by_key(id)
+        return JsonResponse(response, status=status.HTTP_200_OK)
+    except Exception:
+        return JsonResponse({ "error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+# @require_POST
+# def retrieve_file_details(request, id):
+#     data=request.data
+
+#     if not (data.type or data.id):
+#         return JsonResponse({ "error": "Search criteria cannot be null"}, status=status.HTTP_400_BAD_REQUEST)
+#     #todo: to be replaced 
+#     count = 10;
+#     if data.type == "like":
+#         count =  count - 1
+#     else:
+#         count = count + 1
+#     return JsonResponse({ votes: count }, status=status.HTTP_200_OK)
 
 
 @authentication_classes([TokenAuthentication])
